12822acab20d24cb1198b3e6d75518027689dbea
paulocmarquesf <51137526+paulocmarquesf@users.noreply.github.com>
2025-05-17 11:38:57 -0300

    Add files via upload

---
+++ cqr_ames.R
@@ -0,0 +1,39 @@
+# Conformalized Quantile Regression (Ames Housing)
+
+library(tidyverse)
+library(rsample)
+library(ranger)
+library(AmesHousing)
+
+ames <- make_ames() |> rename(y = Sale_Price)
+
+set.seed(42)
+
+split <- initial_split(ames, prop = 0.7)
+
+trn <- training(split)
+tst <- testing(split)
+
+idx_cal <- 1:500
+
+cal <- trn[idx_cal, ]
+trn <- trn[-idx_cal, ]
+
+rf <- ranger(y ~ ., data = trn, quantreg = TRUE)
+
+alpha <- 0.1
+
+alpha_low <- alpha / 2
+alpha_high <- 1 - alpha / 2
+
+q_hat_cal <- predict(rf, data = cal, type = "quantiles", quantiles = c(alpha_low, alpha_high))$predictions
+E <- pmax(q_hat_cal[, 1] - cal$y, cal$y - q_hat_cal[, 2])
+E_hat <- sort(E)[(1 - alpha)*(nrow(cal) + 1)]
+q_hat_tst <- predict(rf, data = tst, type = "quantiles", quantiles = c(alpha_low, alpha_high))$predictions
+
+lower <- q_hat_tst[, 1] - E_hat
+upper <- q_hat_tst[, 2] + E_hat
+
+summary(upper - lower)
+
+mean(lower <= tst$y & tst$y <= upper)
